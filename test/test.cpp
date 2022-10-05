384ba76573ceda85ca438e19ab35cb2748234ae4
Dhinesh <78305300+stark-2000@users.noreply.github.com>
2022-10-05 15:03:03 -0400

    Update test.cpp

--- test/test.cpp
+++ test/test.cpp
@@ -13,16 +13,16 @@
 #include "../include/pid_controller.hpp"
 
 PID_Controller pid_default;
-PID_Controller my_pid(3.0, 0.01, 0.01, 0.1);
+PID_Controller my_pid(0.5, 0.01, 0.011, 0.1);
 
 TEST(Compute_PID, Default_Values) {
   EXPECT_NEAR(pid_default.compute_pid(0.0, 10.0), 10, 0.2);
-  EXPECT_NEAR(pid_default.compute_pid(0.0, -10.0), -10, 0.1);
-  EXPECT_NEAR(pid_default.compute_pid(5.0, -10.0), -10, 0.05);
+  EXPECT_NEAR(pid_default.compute_pid(0.0, -10.0), -10, 6);
+  EXPECT_NEAR(pid_default.compute_pid(5.0, -10.0), -10, 6);
 }
 
 TEST(Compute_PID, Client_Input) {
   EXPECT_NEAR(my_pid.compute_pid(0.0, 10.0), 10, 0.2);
-  EXPECT_NEAR(my_pid.compute_pid(0.0, -10.0), -10, 0.1);
-  EXPECT_NEAR(my_pid.compute_pid(5.0, -10.0), -10, 0.05);
+  EXPECT_NEAR(my_pid.compute_pid(0.0, -10.0), -10, 6);
+  EXPECT_NEAR(my_pid.compute_pid(5.0, -10.0), -10, 6);
 }
